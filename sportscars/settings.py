d493bfd4a15fb5753dfeb9c3e4da5dcec0ec40e0
Renan Ausec <renanausec@MacBook-Air-de-Renan.local>
2024-04-07 20:33:02 -0300

    alterando settings.py

--- sportscars/settings.py
+++ sportscars/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-omvh&h748(3#x+%b#rl4h0j4-=xhsuo+kqo*ae_t2r$#zb$d$m'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
