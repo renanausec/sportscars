006d3cd254d77bd69cb991e9217da24421cbc16c
Renan Ausec <renanausec@MacBook-Air-de-Renan.local>
2024-04-14 14:55:44 -0300

    inquiry e contato 2

--- contato/admin.py
+++ contato/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Contato
+from .models import Inquiry, Contato
 
 # Register your models here.
 
-class ContatoAdmin(admin.ModelAdmin):
+class InquiryAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'email', 'carro_nome', 'cidade', 'create_date')
     list_display_links = ('id', 'nome', 'sobrenome')
     search_fields = ('nome', 'sobrenome', 'email', 'carro_nome')
     list_per_page = 25
 
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'create_date')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'email', 'assunto')
+    list_per_page = 25
+
+admin.site.register(Inquiry, InquiryAdmin)
 admin.site.register(Contato, ContatoAdmin)
