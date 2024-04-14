dc1d8b0a4254bf54d172115128b21b6df466086e
Renan Ausec <renanausec@MacBook-Air-de-Renan.local>
2024-04-14 13:08:35 -0300

    email server

--- sportscars/settings.py
+++ sportscars/settings.py
@@ -163,9 +163,9 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #Email sending
-EMAIL_HOST = ''
-EMAIL_HOST_USER = 'contato@sportscars.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = 'smtp.hostinger.com'
+EMAIL_HOST_USER = 'contato@londrinaseo.com.br'
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
 EMAIL_USE_TLS = True
 
 #whitenoise
