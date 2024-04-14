beeb225566909b337cc09e02644f85e76c4a115b
Renan Ausec <renanausec@MacBook-Air-de-Renan.local>
2024-04-14 14:35:33 -0300

    resolvendo emails contato

--- sportscars/settings.py
+++ sportscars/settings.py
@@ -161,12 +161,14 @@ MESSAGE_TAGS = {
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#Email sending
-EMAIL_HOST = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# #Email sending
+# EMAIL_HOST = ''
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
 
 #whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
